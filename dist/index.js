20b319f21e95c05115343d59e665a5b0e1a0c637
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-15 15:02:15 -0800

    Adds tsconfig file with config options, adds index.ts to demonstrate function before and after compiling

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function add(a, b) {
+    let c = a + b;
+    return c;
+}
+add(1, 2);
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+function add (a: any, b: any) {
+  let c = a + b;
+  return c;
+}
+
+add(1, 2)
