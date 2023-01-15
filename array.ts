d33fc76557e284fe3968c235805857b16e9cc963
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-15 15:38:26 -0800

    Adds tuple set with defined types to array.ts

--- array.ts
+++ array.ts
@@ -1,3 +1,9 @@
 let arr:(number|string)[]
 
 arr = [1, 2, '3']
+
+//tuple example...
+// id-num, name-string, married?-boolean
+let person: [number, string, boolean]
+
+person = [1, 'Kyle', false]
--- dist/array.js
+++ dist/array.js
@@ -1,4 +1,8 @@
 "use strict";
 let arr;
 arr = [1, 2, '3'];
+//tuple example...
+// id-num, name-string, married?-boolean
+let person;
+person = [1, 'Kyle', false];
 //# sourceMappingURL=array.js.map
