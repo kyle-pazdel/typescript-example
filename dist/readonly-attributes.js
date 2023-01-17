73733b3c100811d59add8c2a7682093d8a006c5e
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-17 07:13:57 -0800

    Adds example of interface with readonly attribute to readonly-attributes.ts

---
+++ dist/readonly-attributes.js
@@ -0,0 +1,7 @@
+"use strict";
+let myObject = {
+    id: 1,
+    name: 'Kyle'
+};
+myObject.id = 1;
+//# sourceMappingURL=readonly-attributes.js.map
---
+++ readonly-attributes.ts
@@ -0,0 +1,11 @@
+type myInterface = {
+  readonly id: number,
+  name: string
+}
+
+let myObject:myInterface = {
+  id: 1,
+  name: 'Kyle'
+}
+
+myObject.id = 1;
