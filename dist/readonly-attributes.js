e07beadf25ea4d53a76927f1ccb3da7c53094642
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-17 07:16:59 -0800

    Implements interface/type in class with readonly

--- dist/readonly-attributes.js
+++ dist/readonly-attributes.js
@@ -3,5 +3,11 @@ let myObject = {
     id: 1,
     name: 'Kyle'
 };
-myObject.id = 1;
+// myObject.id = 1;  -- Not valid because id is defined as readonly in interface
+class myClass1 {
+    constructor() {
+        this.id = 1;
+        this.name = 'Kyle';
+    }
+}
 //# sourceMappingURL=readonly-attributes.js.map
--- readonly-attributes.ts
+++ readonly-attributes.ts
@@ -8,4 +8,9 @@ let myObject:myInterface = {
   name: 'Kyle'
 }
 
-myObject.id = 1;
+// myObject.id = 1;  -- Not valid because id is defined as readonly in interface
+
+class myClass1 implements myInterface {
+  id = 1;
+  name = 'Kyle'
+}
