39c1e6301b26dabe0b485c2d3bf7474bb7cd7d37
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-01-17 07:20:20 -0800

    Adds example of void to demonstrate how typescript handles type void for functions

---
+++ dist/function-types-and-voids.js
@@ -0,0 +1,11 @@
+"use strict";
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// Void
+function log(message) {
+    console.log(message);
+    return 1;
+}
+//# sourceMappingURL=function-types-and-voids.js.map
---
+++ function-types-and-voids.ts
@@ -0,0 +1,10 @@
+// Functions
+function addNum(x: number, y: number): number {
+  return x + y
+}
+
+// Void
+function log(message: string | number) : void {
+  console.log(message);
+  return 1;
+} 
